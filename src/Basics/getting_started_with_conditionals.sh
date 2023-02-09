069fa6e92b6e28e7ad27c8e1ca42d5259c3de0eb
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-02-09 19:55:25 +0100

    Update getting_started_with_conditionals.sh

--- src/Basics/getting_started_with_conditionals.sh
+++ src/Basics/getting_started_with_conditionals.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-read answer
+read input
 
-if [[ "$answer"  == 'y' ]] ; then
-    echo 'YES'
-elif [[ "$answer" == 'n' ]] ; then
-    echo 'NO'
+if [[ "$input" == "y" || "$input" == "Y" ]]; then
+    echo "YES"
+fi
+
+if [[ "$input" == "n" || "$input" == "N" ]]; then
+    echo "NO"
 fi
