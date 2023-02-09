71056e3a078688909d1c8f972fbfe5025ee805f0
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-02-09 21:24:08 +0100

    Update compute_the_average.sh

--- src/Basics/compute_the_average.sh
+++ src/Basics/compute_the_average.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
+
 read n
 sum=0
-for (( i = 0; i < n; i++ )); do
-    read x
-    sum=$((sum + x))
+
+for i in $(seq 1 $n); do
+    read number
+    sum=$((sum + number))
 done
 
-avg=$(bc <<< "scale=3;  $sum / $n")
-
-echo $avg
-
-
+echo "scale=3;  $sum / $n" | bc
