e24928e1f5086383b83a1ff7172ec335db09ace8
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-02-12 23:26:59 +0100

    Update lonely_integer.sh

--- src/arrays/lonely_integer.sh
+++ src/arrays/lonely_integer.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
-read N
-if (($N == 1)); then
-    echo $N
-else
-    array=($(cat))
-    x=${array[0]}
-    end=$((N-2))
-    for ((i=1;i<$end;i++))
-    do
-        if [ $x -eq ${array[i]} ]; then
-            x=${array[i+1]}
-        fi
-    done
-    echo $x
-fi
 
+# read and ignore n
+read n
+
+# read the array of integers
+read -a array
+
+# initialize a dictionary to store the count of each element
+declare -A dict
+
+# loop through the array
+for element in "${array[@]}"; do
+  # increment the count of the current element in the dictionary
+  dict[$element]=$((${dict[$element]} + 1))
+done
+
+# loop through the dictionary
+for key in "${!dict[@]}"; do
+  # check if the count of the current key is 1
+  if [[ "${dict[$key]}" == "1" ]]; then
+    # if yes, print the key (which is the number that occurs only once)
+    echo $key
+    break
+  fi
+done
