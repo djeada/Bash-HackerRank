e2e53831daca7269df7fae4dae9f97a84dcf173c
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2026-05-14 21:50:05 +0200

    Improve awk script for line processing
    Refactor awk script to handle empty lines and print last line correctly.

--- src/grep_sed_awk/awk_4.sh
+++ src/grep_sed_awk/awk_4.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 awk '{
     if (NR % 2 == 1)
-        line = $0
-    else
-        print line ";" $0
-}'
+        line=$0
+    else {
+        print line ";" $0 
+        line=""
+    }
+}
+END {
+    if (line!="") {
+        print line ";"
+    }
+}
+' 
